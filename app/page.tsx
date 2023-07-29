8f68e563b795b06aa19e4b72a8fc3e47272694f5
rod608 <rod608ney@gmail.com>
2023-07-29 16:37:37 -0400

    Updated Pokemon data fetching to add images.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-import PokemonGrid from '@/components/ui/PokemonGrid'
+import PokemonGrid from '@/components/PokemonGrid'
 
 async function getPokemonData() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-  return response.json();
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+  const data = await response.json();
+  return addImagesToPokemonData(data.results);
+}
+
+async function addImagesToPokemonData(pokemonList: any) {
+  pokemonList.forEach((pokemon) => {
+    const id = 0o0;
+    pokemon.id = pokemon.url.split('/')[6];
+    pokemon.front_sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+  })
+  return pokemonList;
 }
 
 export default async function Home() {
