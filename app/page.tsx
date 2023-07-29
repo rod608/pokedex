9700bcf008d39c09c4f095431ed09f92fa38a711
rod608 <rod608ney@gmail.com>
2023-07-29 14:24:17 -0400

    Pokemon names shown

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+import PokemonGrid from '@/components/ui/PokemonGrid'
+
 async function getPokemonData() {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon');
   return response.json();
 }
 
 export default async function Home() {
-  const data = await getPokemonData();
-  console.log(data);
+  const pokemonData = await getPokemonData();
 
   return (
     <main>
       <div>
         <h1>Hello, World!</h1>
+        <PokemonGrid pokemonData={pokemonData} />
       </div>
     </main>
   )
