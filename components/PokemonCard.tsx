667aab40155d7698bfb644e26b23f075604f59d1
rod608 <rod608ney@gmail.com>
2023-07-29 21:17:58 -0400

    Added Links to pokemon pages

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import { Pokemon } from "@/types";
+import Link from "next/link";
 import Image from "next/image";
 
 interface PokemonCardProps {
@@ -10,14 +11,18 @@ export default function PokemonCard({ pokemon }: PokemonCardProps) {
     pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
   return (
-    <div className="py-4 flex flex-col">
-      <h2 className="mx-auto">{pokemonName}</h2>
-      <Image
-        src={pokemon.front_sprite}
-        alt={`Image of ${pokemonName}`}
-        width={"100"}
-        height={"100"}
-      ></Image>
-    </div>
+    <Link href={`/${pokemon.name}`}>
+      <div key={pokemon.id} className="border flex justify-center">
+        <div className="py-4 flex flex-col">
+          <h2 className="mx-auto">{pokemonName}</h2>
+          <Image
+            src={pokemon.front_sprite}
+            alt={`Image of ${pokemonName}`}
+            width={"100"}
+            height={"100"}
+          ></Image>
+        </div>
+      </div>
+    </Link>
   );
 }
