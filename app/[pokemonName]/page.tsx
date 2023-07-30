3c4edbc58db896ef011c60a2fad8582bd3d4cbd1
rod608 <rod608ney@gmail.com>
2023-07-30 19:23:04 -0400

    Implemented a base

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -1,5 +1,6 @@
 import { getPokemonData } from "@/lib/pokeAPI";
 import { IndividualPokemon } from "@/types";
+import Image from "next/image";
 
 export default async function PokemonPage({
   params,
@@ -8,12 +9,32 @@ export default async function PokemonPage({
 }) {
   const { pokemonName } = params;
   const pokemonData: IndividualPokemon = await getPokemonData(pokemonName);
+
   console.log(pokemonData);
 
   return (
-    <div>
-      <h1>{pokemonName}</h1>
-      <h1>{pokemonData.id}</h1>
+    <div className="container mt-12">
+      <h1 className="text-center text-4xl capitalize">
+        {pokemonName}{" "}
+        <span className="text-slate-500">
+          {"#" + pokemonData.id.toString().padStart(4, "0")}
+        </span>
+      </h1>
+
+      <div className="grid grid-cols-2 border mt-8">
+        <div className="">
+          <Image
+            src={pokemonData.sprites.front_default}
+            alt={"Picture of " + { pokemonName }}
+            width="650"
+            height="650"
+            className="border mx-auto"
+          />
+          stats
+        </div>
+
+        <div className="flex flex-col border">data</div>
+      </div>
     </div>
   );
 }
