82c58e66314f299ec170806bc46c71dc2bd95075
rod608 <rod608ney@gmail.com>
2023-07-30 16:54:09 -0400

    PokemonData type defined

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -1,13 +1,31 @@
 import { getPokemonData } from "@/lib/pokeAPI"
 
+interface PokemonData {
+    name: string;
+    id: number;
+    sprites: {
+        back_default: string,
+        back_female: string, 
+        back_shiny: string,
+        back_shiny_female: string,
+        front_default: string,
+        front_female: string, 
+        front_shiny: string,
+        front_shiny_female: string
+    };
+    weight: number;
+    height: number;
+}
+
 export default async function PokemonPage({ params }: { params: { pokemonName: string } }) {
   const { pokemonName } = params;
-  const pokemonData = await getPokemonData(pokemonName);
+  const pokemonData: PokemonData = await getPokemonData(pokemonName);
   console.log(pokemonData);
 
   return (
     <div>
-      <h1>{pokemonName === "nidoqueen" ? "Hi baby!!! 😘😘😘" : pokemonName}</h1>
+      <h1>{pokemonName}</h1>
+      <h1>{pokemonData.id}</h1>
     </div>
   );
 }
