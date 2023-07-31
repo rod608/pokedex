2f32c9c97cbabfa3832776bc66c125b7d68b0d02
rod608 <rod608ney@gmail.com>
2023-07-30 21:14:19 -0400

    Added Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " border min-h-screen"}>
+        <nav className="h-20 flex bg-neutral-600 sticky top-0 shadow-lg">
+          <div className="container my-auto">
+            <Link href="/" className="text-slate-50">Home</Link>
+          </div>
+        </nav>
+        {children}
+\      </body>
     </html>
   );
 }
