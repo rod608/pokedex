e02bb9eaef5b991c546da323e5e9c37d6246a18e
rod608 <rod608ney@gmail.com>
2023-08-04 06:33:23 -0400

    Added an Accordion Component

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -4,6 +4,12 @@ import { IndividualPokemon } from "@/types";
 import Image from "next/image";
 import PokemonStat from "@/components/PokemonStat";
 import PokemonType from "@/components/PokemonType";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default async function PokemonPage({
   params,
@@ -35,37 +41,87 @@ export default async function PokemonPage({
             alt={"Picture of " + { pokemonName }}
             width="500"
             height="500"
-            className="border mx-auto"
+            className="mx-auto"
           />
-
-          <div className="mx-10 border bg-slate-100">
-            data
-            <div className="border">
-              Types:
-              {pokemonData.types.map((typeObject) => {
-                return (
-                  <PokemonType
-                    name={typeObject.type.name}
-                    key={pokemonData.id}
-                  />
-                );
-              })}
-            </div>
-            {pokemonData.height}
-            {pokemonData.stats.length}
-          </div>
         </div>
 
         <div className="border bg-slate-100">
-          {pokemonData.stats.map((statObject) => {
-            return (
-              <PokemonStat
-                statName={statObject.stat.name}
-                statValue={statObject.base_stat}
-                key={pokemonData.id}
-              />
-            );
-          })}
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Types</p>
+              </AccordionTrigger>
+              <AccordionContent>
+                {pokemonData.types.map((typeObject) => {
+                  return (
+                    <PokemonType
+                      name={typeObject.type.name}
+                      key={pokemonData.id}
+                    />
+                  );
+                })}
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-2" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Description</p>
+              </AccordionTrigger>
+              <AccordionContent>description</AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-3" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Height</p>
+              </AccordionTrigger>
+              <AccordionContent>{pokemonData.height}</AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-4" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Weight</p>
+              </AccordionTrigger>
+              <AccordionContent>{pokemonData.weight}</AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-5" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Base Stats</p>
+              </AccordionTrigger>
+              <AccordionContent>
+                {pokemonData.stats.map((statObject) => {
+                  return (
+                    <PokemonStat
+                      statName={statObject.stat.name}
+                      statValue={statObject.base_stat}
+                      key={pokemonData.id}
+                    />
+                  );
+                })}
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-6" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Abilities</p>
+              </AccordionTrigger>
+              <AccordionContent>abilities</AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-7" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Sprites</p>
+              </AccordionTrigger>
+              <AccordionContent>Sprites</AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-8" className="px-2">
+              <AccordionTrigger>
+                <p className="font-mono">Moves</p>
+              </AccordionTrigger>
+              <AccordionContent>Moves</AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </div>
     </div>
