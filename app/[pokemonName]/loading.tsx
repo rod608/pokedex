41a4f883be4e6fd5e5025738b4bd41245667bcc4
rod608 <rod608ney@gmail.com>
2023-08-05 14:25:43 -0400

    Skeleton added to Loading page.

--- app/[pokemonName]/loading.tsx
+++ app/[pokemonName]/loading.tsx
@@ -1,5 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function PokemonLoading() {
-    return (
-        <div>Loading...</div>
-    );
+  return (
+    <div className="container mt-12 mb-8 mx-auto">
+      <div className="flex justify-center">
+          <Skeleton className="w-[150px] h-[40px] rounded-full" />
+          <span className="mx-2"></span>
+          <Skeleton className="w-[100px] h-[40px] rounded-full" />
+      </div>
+
+      <Skeleton className="mt-8 p-4 h-[850px] md:h-[535px]" />
+    </div>
+  );
 }
