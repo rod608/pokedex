d9c3eee2796de6dce8a64c74bb7f2d370ac6b2bf
rod608 <rod608ney@gmail.com>
2023-08-05 15:11:15 -0400

    Capitalized stat name

--- components/PokemonStat.tsx
+++ components/PokemonStat.tsx
@@ -11,7 +11,7 @@ export default function PokemonStats({
 }: PokemonStatsProps) {
   return (
     <div className="flex items-stretch mx-auto w-3/4">
-      <h3 className="p-3 w-2/4">
+      <h3 className="p-3 w-2/4 capitalize">
         {statName}: {statValue}
       </h3>
       <Progress className="w-2/4 m-auto" value={statValue} />
